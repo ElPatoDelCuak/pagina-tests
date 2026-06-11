f3b057e4f8d3ef5fece6b4c5678219a8089d77a4
David <194682261+ElPatoDelCuak@users.noreply.github.com>
2026-06-11 09:51:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,12 @@ let userAnswers = [];
 let testTema = '';
 
 function shuffle(arr) {
-  return [...arr].sort(() => Math.random() - .5);
+  const newArr = [...arr];
+  for (let i = newArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+  }
+  return newArr;
 }
 
 function extractQuestions(data) {
